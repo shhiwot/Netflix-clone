71a1d6b55cc364ea4759b586879870ffcf960c17
shhiwot <shhiwot14@gmail.com>
2024-07-02 14:20:08 -0500

    Header And Footer

---
+++ Netflix-project/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./page/Home";
+function App() {
+  return (
+    <>
+      <Home />
+    </>
+  );
+}
+
+export default App;
